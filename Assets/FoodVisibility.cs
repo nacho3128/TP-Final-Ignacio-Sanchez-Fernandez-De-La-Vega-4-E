d1e01d9e25fbfb3b8af80f3a805abe3331b0a2fd
nacho3128 <ignaciosdelavega@gmail.com>
2024-06-19 15:51:15 -0300

    Initial commit

---
+++ Assets/FoodVisibility.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodVisibility : MonoBehaviour
+{
+    public GameObject[] Food;
+    public int CurrentIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (CurrentIndex >= 0)
+            {
+                Food[CurrentIndex].SetActive(false);
+                CurrentIndex--;
+                Food[CurrentIndex].SetActive(true);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(CurrentIndex < Food.Length)
+            {
+                Food[CurrentIndex].SetActive(false);
+                CurrentIndex++;
+                Food[CurrentIndex].SetActive(true);
+            }
+        }
+        if(CurrentIndex > 17 || CurrentIndex<0)
+        {
+            CurrentIndex = 1;
+        }
+    }
+        void DeactivateAll()
+        {
+            for (int i = 0; i < Food.Length; i++)
+            {
+                Food[i].SetActive(false);
+
+
+            }
+        }
+    void ActivateByIndex(int index)
+    {
+        Food[index].SetActive(true);
+    }
+}
